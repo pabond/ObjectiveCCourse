07ef0e732ed85abba9621113960d659456b27e73
Bondar Pavel <bond.p85@gmail.com>
2016-06-16 18:34:28 +0300

    BPVObjectiveC -> add NSString+BPVExtentions file

---
+++ BPVObjectiveC/BPVObjectiveC/Sources/ABC/NSString+BPVExtentions.h
@@ -0,0 +1,15 @@
+//
+//  NSString+BPVExtentions.h
+//  BPVObjectiveC
+//
+//  Created by Bondar Pavel on 6/16/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (BPVExtentions)
+
++ (NSString *)randomString;
+
+@end
