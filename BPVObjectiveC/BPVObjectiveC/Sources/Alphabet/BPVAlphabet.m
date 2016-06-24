aa301761ecb644433c072960acb006b40f8e723f
Bondar Pavel <bond.p85@gmail.com>
2016-06-24 15:52:23 +0300

    BPVAlphabet -> remove implementation seen in video lectures

--- BPVObjectiveC/BPVObjectiveC/Sources/Alphabet/BPVAlphabet.m
+++ BPVObjectiveC/BPVObjectiveC/Sources/Alphabet/BPVAlphabet.m
@@ -19,72 +19,12 @@
 #pragma mark -
 #pragma mark Class Methods Implementation
 
-+ (instancetype)alphabethWithRange:(NSRange)range {
-    return [[[BPVRangeAlphabet alloc] initWithRange:range] autorelease];
-}
-
-+ (instancetype)alphabethWithStrings:(NSArray *)strings {
-    return [[[BPVStringsAlphabet alloc] initWithStrings:strings] autorelease];
-}
-
-+ (instancetype)alphabethWithAlphabets:(NSArray *)alphabets {
-    return [[[BPVClusterAlphabet alloc] initWithStrings:alphabets] autorelease];
-}
-
-+ (instancetype)alphabethWithSymbols:(NSString *)string {
-    return [self alphabethWithStrings:[string symbols]];
-}
-
 #pragma mark -
 #pragma mark Deallocation / Initialisation
 
-- (instancetype)initWithRange:(NSRange)range {
-    [self release];
-    
-    return [[BPVRangeAlphabet alloc] initWithRange:range];
-}
-
-- (instancetype)initWithAlphabets:(NSArray *)alphabets {
-    [self release];
-    
-    return [[BPVClusterAlphabet alloc] initWithAlphabets:alphabets];
-}
-
-- (instancetype)initWithStrings:(NSArray *)strings {
-    [self release];
-    
-    return [[BPVStringsAlphabet alloc] initWithStrings:strings];
-}
-- (instancetype)initWithSymbols:(NSString *)string {
-    return [self initWithStrings:[string symbols]];
-}
-
 #pragma mark -
 #pragma mark Public Implementation
+// should be overwritten
 
-- (NSUInteger)count {
-    [self doesNotRecognizeSelector:_cmd];
-    
-    return 0;
-}
-
-- (NSString *)stringAtIndex:(NSUInteger)index {
-    [self doesNotRecognizeSelector:_cmd];
-
-    return nil;
-}
-
-- (NSString *)objectAtIndexedSubscript:(NSUInteger)index {
-    return [self stringAtIndex:index];
-}
-
-- (NSString *)string {
-    NSMutableString *string = [NSMutableString stringWithCapacity:self.count];
-    for (NSString *symbols in self) {
-        [string appendString:symbols];
-    }
-    
-    return [[string copy] autorelease];
-}
 
 @end
--- BPVObjectiveC/BPVObjectiveC/Sources/Alphabet/Private/BPVClusterAlphabet.m
+++ BPVObjectiveC/BPVObjectiveC/Sources/Alphabet/Private/BPVClusterAlphabet.m
@@ -8,64 +8,7 @@
 
 #import "BPVClusterAlphabet.h"
 
-@interface BPVClusterAlphabet ()
-@property (nonatomic, retain) NSArray *alphabets;
-@property (nonatomic, assign) NSUInteger count;
-
-- (void)initCountWithAlphabets:(NSArray *)alphabets;
-
-@end
-
 @implementation BPVClusterAlphabet
 
-#pragma mark -
-#pragma mark Deallocations / Initialisations
-
-- (void)dealloc {
-    self.alphabets = nil;
-    
-    [super dealloc];
-}
-
-- (instancetype)initWithAlphabets:(NSArray *)alphabets {
-    self = [super init];
-    if (self) {
-        self.alphabets = alphabets;
-        [self initCountWithAlphabets:alphabets];
-    }
-    
-    return self;
-}
-
-- (void)initCountWithAlphabets:(NSArray *)alphabets {
-    NSUInteger count = 0;
-    
-    for (BPVAlphabet *alphabet in alphabets) {
-        count += alphabet.count;
-    }
-    
-    self.count = count;
-}
-
-#pragma mark -
-#pragma mark Public implementations
-
-- (NSString *)stringAtIndex:(NSUInteger)index {
-    NSUInteger count = self.count;
-    if  (index < count) {
-        NSUInteger iteratedIndex = index;
-        
-        for (BPVAlphabet *alphabet in self.alphabets) {
-            count = alphabet.count;
-            if (iteratedIndex < count) {
-                return alphabet[iteratedIndex];
-            }
-            
-            iteratedIndex -= count;
-        }
-    }
-    
-    return nil;
-}
 
 @end
